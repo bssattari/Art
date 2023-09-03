493b8766cd3dd6391bb06d7e72aef63fd08c6353
unknown <b.s.sattari@gmail.com>
2023-09-03 13:13:17 +0300

    Site Ve AdminPanel islemleri sonlandi

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -15,6 +15,7 @@ public class AdminController : Controller
 {
     private readonly ILogger<AdminController> _logger;
     private readonly IWebHostEnvironment webHostEnvironment;
+
     public AdminController(IWebHostEnvironment hostEnvironment, ILogger<AdminController> logger)
     {
         webHostEnvironment = hostEnvironment;
@@ -22,24 +23,189 @@ public class AdminController : Controller
     }
 
     PmitLn2oqDb0001Context db = new PmitLn2oqDb0001Context()!;
-    private object postedData;
-
-    [Route("/admin/slide")]
-    public IActionResult Slide()
+    [AllowAnonymous]
+    [Route("/admin/forgot-password")]
+    public IActionResult ForgotPassword()
     {
-        int id = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+        var model = new IndexViewModel()
+        {
+            Site = db.Sites!.First(),
+        };
+        return View(model);
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    [IgnoreAntiforgeryToken]
+    [Route("/admin/recover-password")]
+    public IActionResult RecoverPassword(User postedData)
+    {
+
+        User user = db.Users!.FirstOrDefault(x => x.Email == postedData.Email && x.Role == "admin")!;
+        Site site = db.Sites.First();
+        Smtp smtp = db.Smtps.First();
+
+        if (user != null)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress(site.Title, smtp.Email));
+            message.To.Add(new MailboxAddress("Admin", user.Email));
+            message.Subject = site.Title + " | Admin Şifre Hatırlatma";
+
+            var body = new StringBuilder();
+            body.AppendLine(site.Title + " | Admin Şifre Hatırlatma");
+            body.AppendLine("");
+            body.AppendLine("------------------------------------------");
+            body.AppendLine("");
+
+            body.AppendLine("Email : " + user.Email);
+            body.AppendLine("Şifre : " + user.Password);
+
+            message.Body = new TextPart("plain")
+            {
+                Text = body.ToString(),
+            };
+
+            Methods.sendEmail(message);
+
+            TempData["Success"] = "Şifreniz e-mail adresinize gönderilmiştir.";
+            return Redirect("/admin/forgot-password");
+        }
+        else
+        {
+            TempData["Danger"] = "Bu e-mail adresi sisteme kayıtlı değildir!";
+            return Redirect("/admin/forgot-password");
+        }
+    }
+
+    [AllowAnonymous]
+    [Route("/admin/signin")]
+    public async Task<IActionResult> SignIn()
+    {
+
+        /* bugün yapılan değişiklik*/
+
+        if (Request.Cookies["email"] != null && Request.Cookies["password"] != null && Request.Cookies["role"] != null)
+        {
+            User user = db.Users!.FirstOrDefault(x => x.Email == Request.Cookies["email"] && x.Password == Request.Cookies["password"] && x.Role == Request.Cookies["role"])!;
+
+            if (user != null)
+            {
+                var claims = new List<Claim>{
+                new Claim("id",user.Id.ToString()),
+                };
+
+                var claimsIdendity = new ClaimsIdentity(claims, "Cookies", user.Role, "role");
+                var claimsPrinciple = new ClaimsPrincipal(claimsIdendity);
+                await HttpContext.SignInAsync(claimsPrinciple);
+                return Redirect("/admin");
+            }
+
+        }
+
+        /* bugün yapılan değişiklik sonu*/
+
 
         var model = new IndexViewModel()
         {
-            User = db.Users!.FirstOrDefault(x => x.Id == id && x.Role == "admin"),
             Site = db.Sites!.First(),
-            Slides = db.Slides!.OrderBy(x => x.Order).ToList(),
         };
+
+        return View(model);
+
+
+
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    [IgnoreAntiforgeryToken]
+    [Route("/admin/signin")]
+    public async Task<IActionResult> SignIn(User postedData)
+    {
+
+        User user = db.Users!.FirstOrDefault(x => x.Email == postedData.Email && x.Password == postedData.Password && x.Role == "admin")!;
+
+        if (user != null)
+        {
+            /* bugün yapılan değişiklik*/
+            if (postedData.Isremember != null && postedData.Isremember == "true")
+            {
+                CookieOptions options = new CookieOptions();
+                options.Expires = DateTime.Now.AddDays(365);
+                Response.Cookies.Append("email", user.Email, options);
+                Response.Cookies.Append("password", user.Password, options);
+                Response.Cookies.Append("role", user.Role, options);
+            }
+
+            /* bugün yapılan değişiklik sonu*/
+
+
+            var claims = new List<Claim>{
+                new Claim("id",user.Id.ToString()),
+            };
+
+            var claimsIdendity = new ClaimsIdentity(claims, "Cookies", user.Role, "role");
+            var claimsPrinciple = new ClaimsPrincipal(claimsIdendity);
+            await HttpContext.SignInAsync(claimsPrinciple);
+            return Redirect("/admin");
+        }
+        else
+        {
+            TempData["Danger"] = "Hatalı Email / Şifre!";
+            return Redirect("/admin/signin");
+        }
+    }
+
+    [Route("/admin/signout")]
+    public async Task<IActionResult> Signout()
+    {
+        await HttpContext.SignOutAsync();
+        /* bugün yapılan değişiklik*/
+        Response.Cookies.Delete("email");
+        Response.Cookies.Delete("password");
+        Response.Cookies.Delete("role");
+        /* bugün yapılan değişiklik sonu*/
+
+
+        TempData["Success"] = "Güvenli Çıkış Yapıldı...";
+
+        return Redirect("/admin/signin");
+    }
+
+    [Route("/admin")]
+    public IActionResult Index()
+    {
+        int uid = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+
+        var model = new IndexViewModel()
+        {
+            User = db.Users!.FirstOrDefault(x => x.Id == uid && x.Role == "admin"),
+            Site = db.Sites!.First(),
+        };
+
 
         return View(model);
     }
 
-    [Route("/admin/slide/isview/{id}")]
+    [Route("/admin/slide")]
+    public IActionResult Slide()
+    {
+        int uid = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+
+        var model = new IndexViewModel()
+        {
+            User = db.Users!.FirstOrDefault(x => x.Id == uid && x.Role == "admin"),
+            Site = db.Sites!.First(),
+            Slides = db.Slides!.OrderBy(x => x.Order).ToList(),
+        };
+
+
+        return View(model);
+    }
+
+
+    [Route("/admin/slide/isview/{id?}")]
     public IActionResult SlideIsview(int id)
     {
         Slide toUpdate = db.Slides.FirstOrDefault(x => x.Id == id)!;
@@ -49,20 +215,101 @@ public class AdminController : Controller
 
         return Content(toUpdate.Isview.ToString()!);
 
-        /*  return Redirect(TempData["Url"]!.ToString()!); */
     }
+
     [Route("/admin/slide/add")]
     public IActionResult SlideAdd()
     {
-        int id = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+        int uid = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
 
         var model = new IndexViewModel()
         {
-            User = db.Users!.FirstOrDefault(x => x.Id == id && x.Role == "admin"),
+            User = db.Users!.FirstOrDefault(x => x.Id == uid && x.Role == "admin"),
+            Site = db.Sites!.First(),
+        };
+
+
+        return View(model);
+    }
+
+    [Route("/admin/slide/edit/{id?}")]
+    public IActionResult SlideEdit(int id)
+    {
+        int uid = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+
+        var model = new IndexViewModel()
+        {
+            User = db.Users!.FirstOrDefault(x => x.Id == uid && x.Role == "admin"),
+            Slide = db.Slides!.FirstOrDefault(x => x.Id == id),
             Site = db.Sites!.First(),
         };
 
         return View(model);
+    }
+
+    [Route("/admin/slide/detail/{id?}")]
+    public IActionResult SlideDetail(int id)
+    {
+        int uid = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
+
+        var model = new IndexViewModel()
+        {
+            User = db.Users!.FirstOrDefault(x => x.Id == uid && x.Role == "admin"),
+            Slide = db.Slides!.FirstOrDefault(x => x.Id == id),
+            Site = db.Sites!.First(),
+        };
+
+        return View(model);
+    }
+
+    [HttpPost]
+    [IgnoreAntiforgeryToken]
+    [Route("/admin/slide/save")]
+    public IActionResult SlideSave(Slide postedData, IFormFile File)
+    {
+        if (postedData.Id != 0)
+        {
+            Slide toUpdate = db.Slides.FirstOrDefault(x => x.Id == postedData.Id)!;
+            if (File != null)
+            {
+                string fileExtension = System.IO.Path.GetExtension(File.FileName);
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "uploads/images/");
+                string fileName = Methods.GenerateUrl(postedData.Title!) + "-slide" + fileExtension;
+                string filePath = Path.Combine(uploadsFolder, fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    File.CopyTo(fileStream);
+                }
+                toUpdate.Image = fileName;
+            }
+            toUpdate.Order = postedData.Order;
+            toUpdate.Title = postedData.Title;
+            toUpdate.Subtitle = postedData.Subtitle;
+            toUpdate.Url = postedData.Url;
+            toUpdate.Target = postedData.Target;
+            db.Entry(toUpdate).CurrentValues.SetValues(toUpdate);
+            TempData["Success"] = "Kayıt güncellendi!";
+            db.SaveChanges();
+        }
+        else
+        {
+            string fileExtension = System.IO.Path.GetExtension(File.FileName);
+            string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "uploads/images/");
+            string fileName = Methods.GenerateUrl(postedData.Title!) + "-slide" + fileExtension;
+            string filePath = Path.Combine(uploadsFolder, fileName);
+            using (var fileStream = new FileStream(filePath, FileMode.Create))
+            {
+                File.CopyTo(fileStream);
+            }
+            postedData.Image = fileName;
+            postedData.Isview = true;
+            db.Slides.Add(postedData);
+            db.SaveChanges();
+
+            TempData["Success"] = "Kayıt eklendi!";
+        }
+
+        return Redirect("/admin/slide");
     }
 
     [Route("/admin/slide/delete/{id?}")]
@@ -74,200 +321,10 @@ public class AdminController : Controller
         TempData["Success"] = "Kayıt silindi!";
         return Redirect("/admin/slide");
     }
- [HttpPost]
-    [IgnoreAntiforgeryToken]
-    [Route("/admin/slide/save")]
-    public IActionResult SlideSave(Slide postedData, IFormFile File)
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
     {
-        string fileExtension = System.IO.Path.GetExtension(File.FileName);
-        string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "uploads/images/");
-        /* string fileName = Guid.NewGuid().ToString() + fileExtension; */
-        string fileName = Methods.GenerateUrl(postedData.Title!)+ "-slid" + fileExtension;
-        string filePath = Path.Combine(uploadsFolder, fileName);
-        using (var fileStream = new FileStream(filePath, FileMode.Create))
-        {
-            File.CopyTo(fileStream);
-        }
-        postedData.Image = fileName;
-        postedData.Isview = true;
-        db.Slides.Add(postedData);
-        db.SaveChanges();
-
-        TempData["Success"] = "Kayıt eklendi!";
-        return Redirect("/admin/slide");
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
-
-        [AllowAnonymous]
-        [Route("/admin/forgot-password")]
-        public IActionResult ForgotPassword()
-        {
-
-            var model = new IndexViewModel()
-            {
-                Site = db.Sites!.First(),
-            };
-
-
-            return View(model);
-        }
-
-        [AllowAnonymous]
-        [HttpPost]
-        [IgnoreAntiforgeryToken]
-        [Route("/admin/recover-password")]
-        public IActionResult RecowerPassword(User postedData)
-        {
-
-            User user = db.Users!.FirstOrDefault(x => x.Email == postedData.Email && x.Role == "admin")!;
-            Site site = db.Sites!.First();
-            Smtp smtp = db.Smtps!.First();
-
-            if (user != null)
-            {
-                var message = new MimeMessage();
-                message.From.Add(new MailboxAddress(site.Title, smtp.Email));
-                message.To.Add(new MailboxAddress("Admin", user.Email));
-                message.Subject = site.Title + " | Admin Şifre Hatırlatma";
-
-                var body = new StringBuilder();
-                body.AppendLine(site.Title + " | Admin Şifre Hatırlatma");
-                body.AppendLine("");
-                body.AppendLine("------------------------------------------");
-                body.AppendLine("");
-
-                body.AppendLine("Email : " + user.Email);
-                body.AppendLine("Şifre : " + user.Password);
-
-                message.Body = new TextPart("plain")
-                {
-                    Text = body.ToString(),
-                };
-
-                Methods.sendEmail(message);
-
-                TempData["Success"] = "Şifreniz e-mail adresine gönderilmiştir!";
-                return Redirect("/admin/forgot-password");
-
-            }
-            else
-            {
-                TempData["Danger"] = "Bu e-mail adresi sisteme kayıtlı değil!";
-                return Redirect("/admin/forgot-password");
-            }
-        }
-
-        [AllowAnonymous]
-        [Route("/admin/signin")]
-        public async Task<IActionResult> SignIn()
-        {
-
-            /* bugün yapılan değişiklik*/
-
-            if (Request.Cookies["email"] != null && Request.Cookies["password"] != null && Request.Cookies["role"] != null)
-            {
-                User user = db.Users!.FirstOrDefault(x => x.Email == Request.Cookies["email"] && x.Password == Request.Cookies["password"] && x.Role == Request.Cookies["role"])!;
-
-                if (user != null)
-                {
-                    var claims = new List<Claim>{
-                new Claim("id",user.Id.ToString()),
-                };
-
-                    var claimsIdendity = new ClaimsIdentity(claims, "Cookies", user.Role, "role");
-                    var claimsPrinciple = new ClaimsPrincipal(claimsIdendity);
-                    await HttpContext.SignInAsync(claimsPrinciple);
-                    return Redirect("/admin");
-                }
-
-            }
-
-            /* bugün yapılan değişiklik sonu*/
-
-
-            var model = new IndexViewModel()
-            {
-                Site = db.Sites!.First(),
-            };
-
-            return View(model);
-
-
-
-        }
-
-        [AllowAnonymous]
-        [HttpPost]
-        [IgnoreAntiforgeryToken]
-        [Route("/admin/signin")]
-        public async Task<IActionResult> SignIn(User postedData)
-        {
-
-            User user = db.Users!.FirstOrDefault(x => x.Email == postedData.Email && x.Password == postedData.Password && x.Role == "admin")!;
-
-            if (user != null)
-            {
-                /* bugün yapılan değişiklik*/
-                if (postedData.Isremember != null && postedData.Isremember == "true")
-                {
-                    CookieOptions options = new CookieOptions();
-                    options.Expires = DateTime.Now.AddDays(365);
-                    Response.Cookies.Append("email", user.Email, options);
-                    Response.Cookies.Append("password", user.Password, options);
-                    Response.Cookies.Append("role", user.Role, options);
-                }
-
-                /* bugün yapılan değişiklik sonu*/
-
-
-                var claims = new List<Claim>{
-                new Claim("id",user.Id.ToString()),
-            };
-
-                var claimsIdendity = new ClaimsIdentity(claims, "Cookies", user.Role, "role");
-                var claimsPrinciple = new ClaimsPrincipal(claimsIdendity);
-                await HttpContext.SignInAsync(claimsPrinciple);
-                return Redirect("/admin");
-            }
-            else
-            {
-                TempData["Danger"] = "Hatalı Email / Şifre!";
-                return Redirect("/admin/signin");
-            }
-        }
-
-        [Route("/admin/signout")]
-        public async Task<IActionResult> Signout()
-        {
-            await HttpContext.SignOutAsync();
-            /* bugün yapılan değişiklik*/
-            Response.Cookies.Delete("email");
-            Response.Cookies.Delete("password");
-            Response.Cookies.Delete("role");
-            /* bugün yapılan değişiklik sonu*/
-
-
-            TempData["Success"] = "Güvenli Çıkış Yapıldı...";
-
-            return Redirect("/admin/signin");
-        }
-        [Route("/admin")]
-        public IActionResult Index()
-        {
-            int id = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "id")!.Value);
-
-            var model = new IndexViewModel()
-            {
-                User = db.Users!.FirstOrDefault(x => x.Id == id && x.Role == "admin"),
-                Site = db.Sites!.First(),
-            };
-
-
-            return View(model);
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+}
--- Models/IndexViewModel.cs
+++ Models/IndexViewModel.cs
@@ -1,5 +1,4 @@
 using Art.Models.Entities;
-using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 
 namespace Art.Models;
@@ -7,57 +6,75 @@ namespace Art.Models;
 public class IndexViewModel
 {
 
-    /* Validation */
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
-    public string? Order { get; set; }
-     [Required(ErrorMessage = "Lutfen doldurunuz!")]
-    public string? Title { get; set; }
-    [Required(ErrorMessage = "Lutfen Seciniz!")]
+    /*Validations*/
+
+    [Required(ErrorMessage = "Lütfen seçiniz!")]
     public IFormFile? File { get; set; }
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    public string? Order { get; set; }
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    public string? Title { get; set; }
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
     public string? Subtitle { get; set; }
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
     public string? Url { get; set; }
-    [Required(ErrorMessage = "Lutfen Seciniz!")]
+
+    [Required(ErrorMessage = "Lütfen seçiniz!")]
     public string? Target { get; set; }
-    
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
     public string? Name { get; set; }
-  
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
     public string? Lastname { get; set; }
 
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
-    [EmailAddress(ErrorMessage = "Gecerli bir Email adresi giriniz!")]
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    [EmailAddress(ErrorMessage = "Geçerli bir e-mail adresi giriniz!")]
     public string? Email { get; set; }
-     [Required(ErrorMessage = "Lutfen doldurunuz!")]
-      public string? Password { get; set; }
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
-    public string? Message { get; set; }
-    [Required(ErrorMessage = "Lutfen doldurunuz!")]
-   
-    public string? Subject { get; set; }
-    public bool? Isremember { get; set; }
 
-    /* Others */
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    public string? Password { get; set; }
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    public string? Message { get; set; }
+
+    [Required(ErrorMessage = "Lütfen doldurunuz!")]
+    public string? Subject { get; set; }
+
+
+
+
+
+    /*Others*/
     public Site? Site { get; set; }
+
+    public Slide? Slide { get; set; }
     public IEnumerable<Slide>? Slides { get; set; }
+
     public Blog? Blog { get; set; }
     public IEnumerable<Blog>? Blogs { get; set; }
+
     public Event? Event { get; set; }
     public IEnumerable<Event>? Events { get; set; }
+
     public Work? Work { get; set; }
     public IEnumerable<Work>? Works { get; set; }
     public IEnumerable<Work>? AllWorks { get; set; }
+
+    public Workcat? Workcat { get; set; }
     public IEnumerable<Workcat>? Workcats { get; set; }
-     public Workcat? Workcat { get; set; }
+
     public IEnumerable<Comment>? Comments { get; set; }
-    /* public Contactform? Contactform { get; set; }
-    public IEnumerable<Contactform>? Contactforms { get; set; } */
     public IEnumerable<Like>? Likes { get; set; }
-    public About? About { get; internal set; }
+
+    public About? About { get; set; }
 
     public User? User { get; set; }
     public IEnumerable<User>? Users { get; set; }
-}
 
+
+}
